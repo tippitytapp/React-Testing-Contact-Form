97aedc4c031f64a4215c57fddb12fa6103b11760
tippitytapp <60143534+tippitytapp@users.noreply.github.com>
2020-04-09 14:11:52 -0400

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import * as rtl from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  rtl.render(<App />);
 });
